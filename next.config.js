c0ff99830093a7e0f6ec6b704b26aff0c007e6be
Susam Ashraf <susam@speedrent.com>
2023-08-03 05:13:18 +0600

    Disable eslint

--- next.config.js
+++ next.config.js
@@ -14,5 +14,10 @@ module.exports = {
     }
 
     return config
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   }
 }
