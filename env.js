60e6d9533e52388bdaa6ac89a05f0289ac6d1cfe
Susam Ashraf <susam@speedrent.com>
2023-09-26 04:11:48 +0600

    xl to json dash

--- env.js
+++ env.js
@@ -1,9 +1,9 @@
 
 
-const BASE_URL = 'http://127.0.0.1:8000'
+// const BASE_URL = 'http://127.0.0.1:8000'
 
 // const BASE_URL = 'http://192.168.0.214:8000'
 
-// const BASE_URL = 'https://invoice.saicoshop.com'
+const BASE_URL = 'https://invoice.saicoshop.com'
 
 exports.BASE_URL = BASE_URL
